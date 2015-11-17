5c8d20180fb95bc8661ada24a6bcf5ae677da3f5
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-17 23:30:12 +1300

    Convert all guesses to lowercase, display only wrong guesses

--- hangman-rails/app/models/turn.rb
+++ hangman-rails/app/models/turn.rb
@@ -1,7 +1,15 @@
 class Turn < ActiveRecord::Base
   belongs_to :game
-  
-  validates :letter, format: { with: /\A[a-zA-Z]+\z/}
+
+  before_validation :lowercase, on: :create
+
+  validates :letter, format: { with: /\A[a-z]+\z/}
   validates :letter, length: { is: 1 }
   validates :letter, uniqueness: {scope: :game_id}
+
+  protected
+
+  def lowercase
+    self.letter.downcase!
+  end
 end
